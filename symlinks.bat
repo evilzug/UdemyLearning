def8c05ea10ffa10ef5944da6a2ebc2e9419adfa
evilzug <evilzug@evilzug.com>
2016-09-12 08:41:23 -0700

    initial commit

---
+++ symlinks.bat
@@ -0,0 +1,4 @@
+@echo off
+mklink /D %~dp0\Lesson2\Assets\Resources\Textures C:\Users\evilz\Desktop\CurrentProjects\UdemyLearningBinary\NumberWizard\Textures
+mklink /D %~dp0\Lesson2\Assets\Resources\Audio C:\Users\evilz\Desktop\CurrentProjects\UdemyLearningBinary\NumberWizard\Audio
+pause
