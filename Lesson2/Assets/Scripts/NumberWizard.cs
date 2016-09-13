47dff5e56057b49e4e639e94171ed29d6f100ae5
evilzug <evilzug@evilzug.com>
2016-09-13 08:41:12 -0700

    completed lesson 1

--- Lesson2/Assets/Scripts/NumberWizard.cs
+++ Lesson2/Assets/Scripts/NumberWizard.cs
@@ -1,36 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NumberWizard : MonoBehaviour {
+public class NumberWizard : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    private int max;
+    private int min;
+    private int guess;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    StartGame();
+	}
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        
+        print("=========================");
         print("Welcome to Number Wizard");
         print("Pick a number in your head, but don't tell me!");
 
-	    int max = 1000;
-	    int min = 1;
+        print("The highest number you can pick is " + max);
+        print("The lowest number you can pick is " + min);
 
-	    print("The highest number you can pick is " + max);
-	    print("The lowest number you can pick is " + min);
-
-	    print("Is the number higher or lower then 500?");
+        print("Is the number higher or lower then " + guess + "?");
         print("Up arrow for higher, down arrow for lower, return for equals.");
-	}
+
+        max = max + 1;
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        print("Higher or lower than " + guess);
+        print("Up arrow for higher, down arrow for lower, return for equals.");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-	        print("UpArrow was pressed");
-	    }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+            min = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            print("DownArrow was pressed");
-        }
-        if (Input.GetKeyDown(KeyCode.Return))
+            max = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.Return))
         {
-            print("Return was pressed");
+            print("I won!");
+            StartGame();
         }
     }
 }
