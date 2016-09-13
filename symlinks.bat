3ccdb48060dc0277a1f555dac78655ded450c6d5
evilzug <evilzug@evilzug.com>
2016-09-13 10:09:00 -0700

    updating symlinks

--- symlinks.bat
+++ symlinks.bat
@@ -1,4 +1,5 @@
 @echo off
 mklink /D %~dp0\Lesson2\Assets\Resources\Textures C:\Users\evilz\Desktop\CurrentProjects\UdemyLearningBinary\NumberWizard\Textures
 mklink /D %~dp0\Lesson2\Assets\Resources\Audio C:\Users\evilz\Desktop\CurrentProjects\UdemyLearningBinary\NumberWizard\Audio
+mklink /D %~dp0\Text101\Assets\Resources\Textures C:\Users\evilz\Desktop\CurrentProjects\UdemyLearningBinary\Text101\Textures
 pause
