ca5a4ad08498608c0f684d5207631d4324165a1a
evilzug <evilzug@evilzug.com>
2016-11-10 16:16:22 -0800

    learning!

---
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name)
+	{
+		Debug.Log("Level load request for "+name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest()
+	{
+		Debug.Log("Quit request");
+		Application.Quit();
+	}
+}
